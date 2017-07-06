4da17d2e30b07b73de2a5d7b548ca8ed4b9bc4f2
Mitchel Pigsley <mpigsley@gmail.com>
2017-07-06 15:48:09 -0500

    Put the sector in the middle of the hex grid (very crude)

--- src/utils/hex-generator.js
+++ src/utils/hex-generator.js
@@ -1,7 +1,11 @@
-const hexWidth = 50;
+const defaultHexWidth = 50;
 const hexPadding = 2;
 
 export default ({ width, height, columns, rows, renderSector }) => {
+  const scaledWidth = Math.min(height / (rows + 4), width / (columns + 4));
+  const horizHexOffset = Math.ceil((((width / scaledWidth) / (Math.sqrt(3) / 2)) - columns) / 2);
+  const vertHexOffset = Math.ceil(((height / scaledWidth) - rows) / 2);
+  const hexWidth = renderSector ? scaledWidth / (Math.sqrt(3) / 2) : defaultHexWidth;
   const hexWidthUnit = hexWidth / 4;
   const hexHeight = (Math.sqrt(3) / 2) * hexWidth;
   const hexHeightUnit = hexHeight / 2;
@@ -20,7 +24,9 @@ export default ({ width, height, columns, rows, renderSector }) => {
         width: hexWidth - hexPadding,
         xOffset,
         yOffset: j % 2 ? minRowHeight + hexHeightUnit : minRowHeight,
-        highlighted: renderSector && i < rows && j < columns,
+        highlighted: renderSector
+          && i > vertHexOffset && i < rows + vertHexOffset
+          && j > horizHexOffset && j < columns + horizHexOffset,
       });
       j += 1;
       isWithinWidth = xOffset + (2 * hexWidthUnit) < width;
