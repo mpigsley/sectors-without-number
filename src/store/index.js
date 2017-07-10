8312a7034e50ecdb6492534db83919aa3345caf5
Mitchel Pigsley <mpigsley@gmail.com>
2017-07-10 08:58:33 -0500

    Don't use redux logger in production

--- src/store/index.js
+++ src/store/index.js
@@ -6,14 +6,19 @@ import { routerReducer, routerMiddleware } from 'react-router-redux';
 
 import reducers from './reducers';
 
+const middleware = [
+  thunk,
+  routerMiddleware(browserHistory),
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
 export default createStore(
   combineReducers({
     ...reducers,
     routing: routerReducer,
   }),
-  applyMiddleware(
-    thunk,
-    createLogger(),
-    routerMiddleware(browserHistory),
-  ),
+  applyMiddleware(...middleware),
 );
