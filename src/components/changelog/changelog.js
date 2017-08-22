52cfe28c17f1ce6f393962bd6097434dd21dcd1c
Mitchel Pigsley <mpigsley@gmail.com>
2017-08-22 09:08:43 -0500

    Make the changelog scrollable

--- src/components/changelog/changelog.js
+++ src/components/changelog/changelog.js
@@ -10,37 +10,41 @@ import './style.css';
 
 export default function Changelog() {
   return (
-    <ContentContainer direction="column" align="center" justify="center">
-      <Header type={HeaderType.header2}>Changelog</Header>
-      {Object.keys(changelog)
-        .map(version => ({
-          ...changelog[version],
-          version,
-        }))
-        .map(({ version, description, changes }) =>
-          <FlexContainer
-            className="Changelog-Item"
-            key={version}
-            noMargin
-            direction="column"
-            align="flexStart"
-          >
-            <Header type={HeaderType.header3}>{`v${version}`}</Header>
-            <span>
-              <b>Description:</b> {description}
-            </span>
-            <p className="Changelog-Updates">
-              <b>Updates:</b>
-            </p>
-            <ul>
-              {changes.map(change =>
-                <li key={change}>
-                  {change}
-                </li>,
-              )}
-            </ul>
-          </FlexContainer>,
-        )}
+    <ContentContainer align="flexStart" justify="center">
+      <FlexContainer direction="column">
+        <Header className="Changelog-Header" type={HeaderType.header2}>
+          Changelog
+        </Header>
+        {Object.keys(changelog)
+          .map(version => ({
+            ...changelog[version],
+            version,
+          }))
+          .map(({ version, description, changes }) =>
+            <FlexContainer
+              className="Changelog-Item"
+              key={version}
+              noMargin
+              direction="column"
+              align="flexStart"
+            >
+              <Header type={HeaderType.header3}>{`v${version}`}</Header>
+              <span>
+                <b>Description:</b> {description}
+              </span>
+              <p className="Changelog-Updates">
+                <b>Updates:</b>
+              </p>
+              <ul>
+                {changes.map(change =>
+                  <li key={change}>
+                    {change}
+                  </li>,
+                )}
+              </ul>
+            </FlexContainer>,
+          )}
+      </FlexContainer>
     </ContentContainer>
   );
 }
