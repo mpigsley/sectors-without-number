ab2d3623228de920677e2db3dbce7972b541a4d9
Mitchel Pigsley <mpigsley@gmail.com>
2017-10-12 09:16:34 -0500

    Make the login modal a custom modal

--- src/primitives/modal/login-modal/index.js
+++ src/primitives/modal/login-modal/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
 
-import Modal from 'primitives/modal/modal';
 import Button from 'primitives/other/button';
+import FlexContainer from 'primitives/container/flex-container';
+
+import './style.css';
 
 export default class ConfirmModal extends Component {
   static propTypes = {
@@ -21,20 +24,30 @@ export default class ConfirmModal extends Component {
   onConfirm = () => {};
 
   render() {
+    const actionText = this.state.isLogin ? 'Log in' : 'Sign up';
     return (
-      <Modal
-        hideHeader
+      <ReactModal
+        contentLabel="Signup & Login"
         isOpen={this.props.isOpen}
         onCancel={this.props.onComplete}
-        title="Signup & Login"
-        actionButtons={[
-          <Button primary key="save" onClick={this.onConfirm}>
-            {this.state.isLogin ? 'Log In' : 'Sign up'}
-          </Button>,
-        ]}
+        className="LoginModal"
+        overlayClassName={{
+          base: 'LoginModal-Overlay',
+        }}
       >
-        Login
-      </Modal>
+        <FlexContainer className="LoginModal-Container" justify="center">
+          <Button className="LoginModal-Facebook">Log In with Facebook</Button>
+        </FlexContainer>
+        <div className="LoginModal-LineContainer">
+          <hr className="LoginModal-Line" />
+          <span className="LoginModal-Or">or </span>
+        </div>
+        <FlexContainer justify="center">
+          <Button primary key="save" onClick={this.onConfirm}>
+            {actionText}
+          </Button>
+        </FlexContainer>
+      </ReactModal>
     );
   }
 }
