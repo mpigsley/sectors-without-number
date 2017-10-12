f38a5c01c336694a5b3a66e4b0de105376183150
Mitchel Pigsley <mpigsley@gmail.com>
2017-10-12 10:01:52 -0500

    Add form elements to login and signup

--- src/primitives/modal/login-modal/index.js
+++ src/primitives/modal/login-modal/index.js
@@ -6,6 +6,8 @@ import { X } from 'react-feather';
 
 import Button from 'primitives/other/button';
 import FlexContainer from 'primitives/container/flex-container';
+import Label from 'primitives/form/label';
+import Input from 'primitives/form/input';
 
 import './style.css';
 
@@ -21,9 +23,35 @@ export default class ConfirmModal extends Component {
 
   state = {
     isLogin: true,
+    email: '',
+    password: '',
+    confirm: '',
   };
 
-  onConfirm = () => {};
+  onConfirm = () => {
+    this.props.onComplete();
+  };
+
+  onEditText = ({ target }) => {
+    this.setState({ [target.dataset.key]: target.value });
+  };
+
+  renderPasswordConfirm() {
+    return (
+      <div>
+        <Label htmlFor="confirm">Confirm Password</Label>
+        <Input
+          id="confirm"
+          name="confirm"
+          data-key="confirm"
+          type="password"
+          placeholder="Confirm"
+          value={this.state.confirm}
+          onChange={this.onEditText}
+        />
+      </div>
+    );
+  }
 
   render() {
     const actionText = this.state.isLogin ? 'Log in' : 'Sign up';
@@ -43,7 +71,12 @@ export default class ConfirmModal extends Component {
           size={30}
         />
         <FlexContainer justify="center">
-          <Button className="LoginModal-Facebook">Log In with Facebook</Button>
+          <Button
+            className="LoginModal-Facebook"
+            onClick={this.props.onComplete}
+          >
+            Log In with Facebook
+          </Button>
         </FlexContainer>
         <div className="LoginModal-LineContainer">
           <hr className="LoginModal-Line" />
@@ -67,10 +100,38 @@ export default class ConfirmModal extends Component {
             Sign Up
           </button>
         </FlexContainer>
-        <FlexContainer className="LoginModal-FormContainer" justify="center">
-          <Button primary key="save" onClick={this.onConfirm}>
-            {actionText}
-          </Button>
+        <FlexContainer
+          className="LoginModal-FormContainer"
+          justify="center"
+          direction="column"
+        >
+          <Label noPadding htmlFor="email">
+            Email
+          </Label>
+          <Input
+            id="email"
+            name="email"
+            data-key="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.onEditText}
+          />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            data-key="password"
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.onEditText}
+          />
+          {this.state.isLogin ? undefined : this.renderPasswordConfirm()}
+          <FlexContainer className="LoginModal-Submit" justify="center">
+            <Button primary key="submit" onClick={this.onConfirm}>
+              {actionText}
+            </Button>
+          </FlexContainer>
         </FlexContainer>
       </ReactModal>
     );
