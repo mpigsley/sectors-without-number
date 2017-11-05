aa965eb66e30786184364fc8cfc170fe7c217f14
Mitchel Pigsley <mpigsley@gmail.com>
2017-11-05 09:00:46 -0600

    Dont show sectors that aren't cloud saves on home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
 
 import { generateSector } from 'store/actions/sector.actions';
+import { getUserSectors } from 'store/selectors/sector.selectors';
 import Home from './home';
 
 const mapStateToProps = state => ({
-  saved: state.sector.saved,
+  saved: getUserSectors(state),
 });
 
 const mapDispatchToProps = dispatch => ({
