728e8200edb3fbe1a6291c0f672380dd3b3d303e
Mitchel Pigsley <mpigsley@gmail.com>
2018-01-10 12:31:31 -0600

    Make text slightly smaller

--- src/components/sidebar-navigation/sidebar-navigation.js
+++ src/components/sidebar-navigation/sidebar-navigation.js
@@ -141,7 +141,7 @@ export default function SidebarNavigation({
           </ButtonLink>
           <span className="SidebarNavigation-Spacer" />
           <ButtonLink minimal to="/changelog">
-            Changlog
+            Changelog
           </ButtonLink>
           <span className="SidebarNavigation-Spacer" />
           <ButtonLink
