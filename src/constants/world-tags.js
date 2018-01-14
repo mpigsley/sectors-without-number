944e0459faafc44cdd23866761eec4dd869e8d69
Mitchel Pigsley <mpigsley@gmail.com>
2018-01-14 12:59:00 -0600

    Spell irresistibly correctly

--- src/constants/world-tags.js
+++ src/constants/world-tags.js
@@ -543,7 +543,7 @@ export default {
     key: worldTagKeys.culturalPower,
     name: 'Cultural Power',
     description:
-      'The world is a considerable cultural power in the sector, producing music, art, philosophy, or some similar intangible that their neighbors find irresistably attractive. Other worlds might have a profound degree of cultural cachet as the inheritor of some venerable artistic tradition.',
+      'The world is a considerable cultural power in the sector, producing music, art, philosophy, or some similar intangible that their neighbors find irresistibly attractive. Other worlds might have a profound degree of cultural cachet as the inheritor of some venerable artistic tradition.',
     enemies: [
       'murderously eccentric artist',
       'crazed fan',
