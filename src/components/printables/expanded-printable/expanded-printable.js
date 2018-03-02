e4cd3bb686fed4f1b8bf14c5ef08ff5e05dfc6a0
Mitchel Pigsley <mpigsley@gmail.com>
2018-03-02 12:06:05 -0600

    Show entity information on right side of full page printable

--- src/components/printables/expanded-printable/expanded-printable.js
+++ src/components/printables/expanded-printable/expanded-printable.js
@@ -11,18 +11,86 @@ import Entities from 'constants/entities';
 import './style.css';
 import '../style.css';
 
-const renderEntity = (key, entityType, entity) => (
-  <FlexContainer key={key} className="ExpandedPrintable-Entity">
-    <FlexContainer align="baseline" className="ExpandedPrintable-Header">
-      <Header type={HeaderType.header2} dark>
-        {entity.name}
-      </Header>
-      <Header type={HeaderType.header4} dark className="ExpandedPrintable-Type">
-        ({Entities[entityType].name})
-      </Header>
+const renderEntity = (entityId, entityType, entity) => {
+  const conf = Entities[entityType];
+
+  const blockAttributes = [];
+  if (entity.tags) {
+    blockAttributes.push(
+      <div key="tags">
+        <b>Tags: </b>
+        {entity.tags}
+      </div>,
+    );
+  }
+  if ((conf.attributes || []).length) {
+    conf.attributes.filter(({ key }) => entity[key]).forEach(({ key, name }) =>
+      blockAttributes.push(
+        <div key={key}>
+          <b>{name}: </b>
+          {entity[key]}
+        </div>,
+      ),
+    );
+  }
+  if (entity.neighbors) {
+    blockAttributes.push(
+      <div key="neighbors">
+        <b>Neighbors: </b>
+        {entity.neighbors}
+      </div>,
+    );
+  }
+  if (entity.children) {
+    blockAttributes.push(
+      <div key="children">
+        <b>Children: </b>
+        {entity.children}
+      </div>,
+    );
+  }
+  if (entity.description) {
+    blockAttributes.push(
+      <div key="description">
+        <b>Description: </b>
+        {entity.description}
+      </div>,
+    );
+  }
+  let attrBlock = null;
+  if (blockAttributes.length) {
+    attrBlock = (
+      <FlexContainer justify="flexEnd">
+        <FlexContainer className="ExpandedPrintable-Block" direction="column">
+          {blockAttributes}
+        </FlexContainer>
+      </FlexContainer>
+    );
+  }
+
+  return (
+    <FlexContainer
+      key={entityId}
+      direction="column"
+      className="ExpandedPrintable-Entity"
+    >
+      <FlexContainer align="baseline" className="ExpandedPrintable-Header">
+        <Header type={HeaderType.header2} dark>
+          {entity.name}
+        </Header>
+        <Header
+          type={HeaderType.header4}
+          dark
+          className="ExpandedPrintable-Type"
+        >
+          ({conf.name}
+          {entity.location ? ` - ${entity.location}` : ''})
+        </Header>
+      </FlexContainer>
+      {attrBlock}
     </FlexContainer>
-  </FlexContainer>
-);
+  );
+};
 
 const renderEntities = entities =>
   map(entities, (entityList, entityType) =>
