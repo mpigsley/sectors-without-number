ad9c20baf90d450693bca91b13ae8bcfe1cfbf14
Mitchel Pigsley <mpigsley@gmail.com>
2018-03-09 08:47:08 -0600

    Build out entity overview table

--- src/components/overview-table/overview-table.js
+++ src/components/overview-table/overview-table.js
@@ -1,19 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { values } from 'lodash';
 
+import Header, { HeaderType } from 'primitives/text/header';
 import FlexContainer from 'primitives/container/flex-container';
+import Table from 'primitives/other/table';
+import Entities from 'constants/entities';
+import { sortByKey } from 'utils/common';
 
 import './style.css';
 
-export default function OverviewTable() {
+const getColumnsFromType = entityType => {
+  const common = [{ accessor: 'name', Header: 'Name' }];
+  if (Entities[entityType].topLevel) {
+    return [
+      ...common,
+      { accessor: 'location', Header: 'Location', centered: true },
+      { accessor: 'children', Header: 'Children', centered: true },
+      { accessor: 'neighbors', Header: 'Neighbors' },
+    ];
+  }
+  const columns = [...common, { accessor: 'parent', Header: 'Parent' }];
+  if (Entities[entityType].children.length) {
+    columns.splice(2, 0, {
+      accessor: 'children',
+      Header: 'Children',
+      centered: true,
+    });
+  }
+  if ((Entities[entityType].attributes || []).length) {
+    Entities[entityType].attributes.forEach(({ key, name }) => {
+      columns.push({ accessor: key, Header: name });
+    });
+  }
+  if (Entities[entityType].tags) {
+    columns.push({ accessor: 'tags', Header: 'Tags' });
+  }
+  return columns;
+};
+
+export default function OverviewTable({ entities, routeParams }) {
   return (
     <FlexContainer
       flex="3"
       direction="column"
-      align="center"
-      justify="center"
+      align="flexStart"
       className="OverviewTable"
     >
-      Table
+      <Header
+        type={HeaderType.header3}
+        className="CondensedPrintable-EntityTitle"
+      >
+        {Entities[routeParams.entityType].name}
+      </Header>
+      <Table
+        className="CondensedPrintable-Table"
+        dataIdAccessor="key"
+        columns={getColumnsFromType(routeParams.entityType)}
+        data={values(entities[routeParams.entityType]).sort(sortByKey('name'))}
+      />
     </FlexContainer>
   );
 }
+
+OverviewTable.propTypes = {
+  entities: PropTypes.shape().isRequired,
+  routeParams: PropTypes.shape({
+    entityType: PropTypes.string.isRequired,
+  }).isRequired,
+};
