2ee63e995301d30a768db85947fe5e2826cd24d2
Mitchel Pigsley <mpigsley@gmail.com>
2018-03-13 08:19:47 -0500

    Add way to export overview tables as CSV files

--- src/components/overview-table/index.js
+++ src/components/overview-table/index.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux';
 
-import { getPrintableEntities } from 'store/selectors/entity.selectors';
+import {
+  getPrintableEntities,
+  getCurrentSector,
+} from 'store/selectors/entity.selectors';
 import OverviewTable from './overview-table';
 
 const mapStateToProps = state => ({
   entities: getPrintableEntities(state),
+  currentSector: getCurrentSector(state),
 });
 
 export default connect(mapStateToProps)(OverviewTable);
