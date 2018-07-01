eff17ea307eeaf26e2d06f50ec4d4d1092c9f9b5
Mitchel Pigsley <mpigsley@gmail.com>
2018-07-01 10:45:40 -0500

    Delete sector objects including layers when a sector is deleted

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
 
 const saveEntities = require('./src/save-entities');
-const deleteNavigation = require('./src/delete-navigation');
+const deleteSectorObjects = require('./src/delete-sector-objects');
 
 admin.initializeApp();
 
 exports.saveEntities = saveEntities;
-exports.deleteNavigation = deleteNavigation;
+exports.deleteSectorObjects = deleteSectorObjects;
