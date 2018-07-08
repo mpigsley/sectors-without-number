4b8bade85b47c75b5066c667305e6fc73e1dbfc1
Mitchel Pigsley <mpigsley@gmail.com>
2018-07-08 16:01:08 -0500

    Glitched effect on title

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 
-import FlexContainer from 'primitives/container/flex-container';
 import Header, { HeaderType } from 'primitives/text/header';
 import StarBackground from 'components/star-background';
 
@@ -10,22 +9,25 @@ export default function Home() {
   return (
     <Fragment>
       <StarBackground>
-        <FlexContainer
-          direction="column"
-          justify="center"
-          className="Home-Header"
-        >
-          <Header
-            noMargin
-            type={HeaderType.header1}
-            className="Home-MainHeader"
-          >
-            Sectors Without Number
-          </Header>
-          <Header type={HeaderType.header2} className="Home-SubHeader">
-            Sector Generator
-          </Header>
-        </FlexContainer>
+        <div className="Home-Header">
+          <div className="Home-Glitch">
+            <Header
+              noMargin
+              type={HeaderType.header1}
+              className="Home-MainHeader"
+              data-text="Sectors Without Number"
+            >
+              Sectors Without Number
+            </Header>
+            <Header
+              type={HeaderType.header2}
+              className="Home-SubHeader"
+              data-text="Sector Generator"
+            >
+              Sector Generator
+            </Header>
+          </div>
+        </div>
       </StarBackground>
     </Fragment>
   );
