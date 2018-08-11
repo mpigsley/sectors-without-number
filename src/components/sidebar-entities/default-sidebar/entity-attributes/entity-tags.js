8b2c177d19e2f16513f99ac64a365b17c790d94d
Jaakko Ojalehto <jaakko@ojalehto.fi>
2018-08-12 00:27:01 +0300

    Lint

--- src/components/sidebar-entities/default-sidebar/entity-attributes/entity-tags.js
+++ src/components/sidebar-entities/default-sidebar/entity-attributes/entity-tags.js
@@ -63,7 +63,7 @@ export default function EntityTags({
             updateEntityInEdit({
               attributes: { tags: pull(entityTags, tag) },
               visibility: { [`tag.${tag}`]: undefined },
-           })
+            })
           }
         />
         <Dropdown
