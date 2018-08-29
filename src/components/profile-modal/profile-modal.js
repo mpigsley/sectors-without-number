4679362de159c2d20a090566636ba6aa92d50493
Mitchel Pigsley <mpigsley@gmail.com>
2018-08-29 07:33:33 -0500

    Use the labeled input in the profile modal

--- src/components/profile-modal/profile-modal.js
+++ src/components/profile-modal/profile-modal.js
@@ -2,13 +2,13 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { intlShape, FormattedMessage } from 'react-intl';
 
+import LabeledInput from 'primitives/form/labeled-input';
 import Button from 'primitives/other/button';
 import Modal from 'primitives/modal/modal';
-import Label from 'primitives/form/label';
-import Input from 'primitives/form/input';
-import Dropdown from 'primitives/form/dropdown';
 import LOCALES from 'constants/locale';
 import { map } from 'constants/lodash';
+
+import './style.css';
 
 export default function ProfileModal({
   closeEditModal,
@@ -30,27 +30,29 @@ export default function ProfileModal({
           </Button>,
         ]}
       >
-        <Label noPadding htmlFor="username">
-          <FormattedMessage id="misc.username" />
-        </Label>
-        <Input
-          id="username"
-          name="username"
-          value={form.displayName || ''}
-          onChange={({ target }) => updateUserForm('displayName', target.value)}
-        />
-        <Label htmlFor="language">
-          <FormattedMessage id="misc.language" />
-        </Label>
-        <Dropdown
-          dropUp
-          id="language"
-          name="language"
-          clearable={false}
-          value={form.locale}
-          options={map(LOCALES, ({ value, name }) => ({ value, label: name }))}
-          onChange={({ value }) => updateUserForm('locale', value)}
-        />
+        <div className="ProfileModel-Content">
+          <LabeledInput
+            isVertical
+            label="misc.username"
+            value={form.displayName || ''}
+            onChange={({ target }) =>
+              updateUserForm('displayName', target.value)
+            }
+          />
+          <LabeledInput
+            isVertical
+            label="misc.language"
+            dropUp
+            type="dropdown"
+            clearable={false}
+            value={form.locale}
+            options={map(LOCALES, ({ value, name }) => ({
+              value,
+              label: name,
+            }))}
+            onChange={({ value }) => updateUserForm('locale', value)}
+          />
+        </div>
       </Modal>
     </Fragment>
   );
