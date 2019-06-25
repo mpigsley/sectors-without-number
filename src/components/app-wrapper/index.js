8dece0d0ac351e0593193a3d32643c1f7e35a6ff
Mitchel Pigsley <mpigsley@gmail.com>
2019-06-25 17:13:47 -0500

    Get the blocked render updates working with latest router

--- src/components/app-wrapper/index.js
+++ src/components/app-wrapper/index.js
@@ -1,9 +1,9 @@
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 import {
   userLocaleSelector,
   userModelLocaleSelector,
+  routerLocationSelector,
 } from 'store/selectors/base.selectors';
 
 import AppWrapper from './app-wrapper';
@@ -11,6 +11,7 @@ import AppWrapper from './app-wrapper';
 const mapStateToProps = state => ({
   userLocale: userModelLocaleSelector(state),
   locale: userLocaleSelector(state),
+  location: routerLocationSelector(state),
 });
 
-export default withRouter(connect(mapStateToProps)(AppWrapper));
+export default connect(mapStateToProps)(AppWrapper);
